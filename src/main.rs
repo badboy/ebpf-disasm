4366358ccd877d878226e73a8809dc4aaa2767e0
Jan-Erik Rediger <janerik@fnordig.de>
2017-01-16 15:52:48 +0100

    Simple ebpf disassembler in Rust

---
+++ src/main.rs
@@ -0,0 +1,50 @@
+extern crate clap;
+extern crate elf;
+extern crate rbpf;
+
+use std::process;
+use clap::{Arg, App};
+
+fn main() {
+    let matches = App::new("ebpf-disasm")
+        .version("1.0")
+        .author("Jan-Erik Rediger")
+        .about("Output disassembled eBPF read from an ELF file")
+        .arg(Arg::with_name("section")
+             .short("s")
+             .long("section")
+             .value_name("SECTION")
+             .help("Specify a section name")
+             .takes_value(true))
+        .arg(Arg::with_name("INPUT")
+             .help("Sets the input ELF file to use")
+             .required(true)
+             .index(1))
+        .get_matches();
+
+    let section = matches.value_of("section").unwrap_or(".classifier");
+    let input_file = matches.value_of("INPUT").unwrap();
+
+    let file = match elf::File::open_path(&input_file) {
+        Ok(f) => f,
+        Err(e) => {
+            println!("Could not read elf file.");
+            println!("Error: {:?}", e);
+            process::exit(1);
+        }
+    };
+
+    let text_scn = match file.get_section(&section) {
+        Some(s) => s,
+        None => {
+            println!("Failed to lookup '{}' section.", section);
+            process::exit(1);
+        }
+    };
+
+    let prog = &text_scn.data;
+
+    for insn in rbpf::disassembler::to_insn_vec(prog) {
+        println!("{}", insn.desc.replace(" ", "\t"));
+    }
+}
