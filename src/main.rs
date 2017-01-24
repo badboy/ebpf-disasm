0ae227468d34734f88e8e43450bd0eb94b619e0d
Jan-Erik Rediger <janerik@fnordig.de>
2017-01-24 14:37:05 +0100

    Option to output as a C-compatible bpf_insn array

--- src/main.rs
+++ src/main.rs
@@ -24,11 +24,21 @@ fn main() {
             .short("b")
             .long("bytecode")
             .help("Only show raw bytecode"))
+        .arg(Arg::with_name("ccode")
+            .short("c")
+            .long("ccode")
+            .help("Only show a C compatible bpf_insn array"))
         .get_matches();
 
     let section = matches.value_of("section").unwrap_or(".classifier");
     let input_file = matches.value_of("INPUT").unwrap();
     let show_bytecode = matches.is_present("bytecode");
+    let show_ccode = matches.is_present("ccode");
+
+    if show_bytecode && show_ccode {
+        println!("Can't show both bytecode and C code.");
+        process::exit(1);
+    }
 
     let file = match elf::File::open_path(&input_file) {
         Ok(f) => f,
@@ -56,6 +66,23 @@ fn main() {
             }
             println!("");
         }
+    } else if show_ccode {
+        println!("struct bpf_insn prog[] = {{");
+        for insn in prog.chunks(8) {
+            print!("\t{{ ");
+            print!(".code = 0x{:>02x}, ", insn[0]);
+            print!(".dst_reg = 0x{:>x}, ", insn[1]&0x0f);
+            print!(".src_reg = 0x{:>x}, ", insn[1]>>4);
+            let off = (insn[2] as u16)<<8 | (insn[3] as u16);
+            print!(".off = 0x{:>04x}, ", off);
+            let imm = (insn[7] as u32)<<24 |
+                (insn[6] as u32)<<16 |
+                (insn[5] as u32)<<8 |
+                (insn[4] as u32)<<0;
+            print!(".imm = 0x{:>08x}", imm);
+            println!(" }},");
+        }
+        println!("}};");
     } else {
         for insn in rbpf::disassembler::to_insn_vec(prog) {
             println!("{}", insn.desc.replace(" ", "\t"));
